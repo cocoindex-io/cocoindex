f9e9bec186002c987d21040d839d999eeaa5ced7
Charles Dong <chardon_cs@proton.me>
2025-05-09 17:37:44 +0800

    Use reversed iteration for union type matching

--- src/utils/union.rs
+++ src/utils/union.rs
@@ -73,50 +73,50 @@ impl ParseStr for BTreeSet<BasicValueType> {
 
     /// Try parsing the str value to each possible type, and return the first successful result
     fn parse_str(&self, value: &str) -> Result<BasicValue> {
-        if self.contains(&BasicValueType::Uuid) {
-            match value.parse().map(BasicValue::Uuid) {
-                Ok(ret) => return Ok(ret),
-                Err(_) => {}
+        // Try parsing the value in the reversed order of the enum elements
+        for typ in self.iter().rev() {
+            match typ {
+                BasicValueType::Uuid => {
+                    match value.parse().map(BasicValue::Uuid) {
+                        Ok(ret) => return Ok(ret),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::OffsetDateTime => {
+                    match value.parse().map(BasicValue::OffsetDateTime) {
+                        Ok(ret) => return Ok(ret),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::LocalDateTime => {
+                    match value.parse().map(BasicValue::LocalDateTime) {
+                        Ok(ret) => return Ok(ret),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::Date => {
+                    match value.parse().map(BasicValue::Date) {
+                        Ok(ret) => return Ok(ret),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::Time => {
+                    match value.parse().map(BasicValue::Time) {
+                        Ok(ret) => return Ok(ret),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::Json => {
+                    match serde_json::Value::from_str(value) {
+                        Ok(ret) => return Ok(BasicValue::Json(ret.into())),
+                        Err(_) => {}
+                    }
+                }
+                BasicValueType::Str => {
+                    return Ok(BasicValue::Str(Arc::from(value)));
+                }
+                _ => {}
             }
-        }
-
-        if self.contains(&BasicValueType::OffsetDateTime) {
-            match value.parse().map(BasicValue::OffsetDateTime) {
-                Ok(ret) => return Ok(ret),
-                Err(_) => {}
-            }
-        }
-
-        if self.contains(&BasicValueType::LocalDateTime) {
-            match value.parse().map(BasicValue::LocalDateTime) {
-                Ok(ret) => return Ok(ret),
-                Err(_) => {}
-            }
-        }
-
-        if self.contains(&BasicValueType::Date) {
-            match value.parse().map(BasicValue::Date) {
-                Ok(ret) => return Ok(ret),
-                Err(_) => {}
-            }
-        }
-
-        if self.contains(&BasicValueType::Time) {
-            match value.parse().map(BasicValue::Time) {
-                Ok(ret) => return Ok(ret),
-                Err(_) => {}
-            }
-        }
-
-        if self.contains(&BasicValueType::Json) {
-            match serde_json::Value::from_str(value) {
-                Ok(ret) => return Ok(BasicValue::Json(ret.into())),
-                Err(_) => {}
-            }
-        }
-
-        if self.contains(&BasicValueType::Str) {
-            return Ok(BasicValue::Str(Arc::from(value)));
         }
 
         anyhow::bail!("Cannot parse \"{}\"", value)
