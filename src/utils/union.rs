fe4941b8c367d8f116e6223552f599b0e26dd558
Charles Dong <chardon_cs@proton.me>
2025-05-25 10:28:46 +0800

    Revert "Use `Vec` to remove auto-sort"
    This reverts commit d19411719c60c6f56db0414cf7345bf8352e396f.

--- src/utils/union.rs
+++ src/utils/union.rs
@@ -3,17 +3,17 @@ use crate::{base::schema::BasicValueType, prelude::*};
 /// Union type helper storing an auto-sorted set of types excluding `Union`
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
 pub struct UnionType {
-    types: Vec<BasicValueType>,
+    types: BTreeSet<BasicValueType>,
 }
 
 impl UnionType {
-    pub fn types(&self) -> &[BasicValueType] {
-        self.types.as_slice()
+    pub fn types(&self) -> &BTreeSet<BasicValueType> {
+        &self.types
     }
 }
 
-impl Into<Vec<BasicValueType>> for UnionType {
-    fn into(self) -> Vec<BasicValueType> {
+impl Into<BTreeSet<BasicValueType>> for UnionType {
+    fn into(self) -> BTreeSet<BasicValueType> {
         self.types
     }
 }
