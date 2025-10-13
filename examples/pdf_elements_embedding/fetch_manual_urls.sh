6f91d97c68fe99f0767c781f7fdf2b671c74cf62
Jiangzhou <jiangzhou@cocoindex.io>
2025-10-12 23:36:20 -0700

    example: swap one example file with higher quality one (#1181)

--- examples/pdf_elements_embedding/fetch_manual_urls.sh
+++ examples/pdf_elements_embedding/fetch_manual_urls.sh
@@ -3,8 +3,8 @@
 URLS=(
     https://www.catan.com/sites/default/files/2021-06/catan_base_rules_2020_200707.pdf
     https://michalskig.wordpress.com/wp-content/uploads/2010/10/manilaenglishgame_133_gamerules.pdf
-    https://fgbradleys.com/wp-content/uploads/rules/Carcassonne-rules.pdf
     https://cdn.1j1ju.com/medias/2c/f9/7f-ticket-to-ride-rulebook.pdf
+    https://cdn.1j1ju.com/medias/0c/93/d6-stone-age-the-expansion-rulebook.pdf
 )
 
 OUTPUT_DIR="source_files"
