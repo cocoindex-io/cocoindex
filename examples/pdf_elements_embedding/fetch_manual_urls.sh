3870daad992c26b3af0b0fbcfc119b7403123f7b
Jiangzhou <jiangzhou@cocoindex.io>
2025-10-23 22:34:29 -0700

    chore: swap an example PDF (mistakenly swapped by commit 6f91d97) (#1217)

--- examples/pdf_elements_embedding/fetch_manual_urls.sh
+++ examples/pdf_elements_embedding/fetch_manual_urls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 URLS=(
-    https://www.catan.com/sites/default/files/2021-06/catan_base_rules_2020_200707.pdf
+    https://fgbradleys.com/wp-content/uploads/rules/Carcassonne-rules.pdf
     https://michalskig.wordpress.com/wp-content/uploads/2010/10/manilaenglishgame_133_gamerules.pdf
     https://cdn.1j1ju.com/medias/2c/f9/7f-ticket-to-ride-rulebook.pdf
     https://cdn.1j1ju.com/medias/0c/93/d6-stone-age-the-expansion-rulebook.pdf
