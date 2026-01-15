f735ab8f8e182f0df801d1badd087b37e9aca765
Jiangzhou <jiangzhou@cocoindex.io>
2026-01-15 08:16:13 -0800

    fix: use `docs-v1` instead of `docs_v1` in URL (#1494)

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   url: 'https://cocoindex.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs_v1/',
+  baseUrl: '/docs-v1/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
