81af87f7fbc89cc0903855f310ddd401fc616a8e
Yaxin Cheng <Yaxin.Cheng@iCloud.com>
2026-07-20 17:21:21 -0700

    Introduce GPU reservation for GPUs that are not currently available. The current strategy is to find GPUs with the shortest waitlist

--- rust/utils/src/gpu_pool.rs
+++ rust/utils/src/gpu_pool.rs
@@ -1,7 +1,7 @@
 use anyhow::{Error, Result};
-use itertools::Itertools;
-use std::{collections::LinkedList, num::NonZeroUsize};
-use tokio::sync::{Mutex, oneshot};
+use std::collections::HashSet;
+use std::{collections::VecDeque, num::NonZeroUsize};
+use tokio::sync::{oneshot, Mutex};
 
 /// Tracks fractional GPU capacity across multiple GPUs.
 ///
@@ -19,40 +19,17 @@ pub struct GPUPool {
 
 struct GPUPoolState {
     capacity: Vec<f32>,
-    waiters: LinkedList<Waiter>,
+    reserved: Vec<VecDeque<(f32, oneshot::Sender<()>)>>,
 }
-
-/// A waiter for a GPU acquisition.
-struct Waiter {
-    desired_capacity: DesiredCapacity,
-    sender: oneshot::Sender<AcquiredGPUs>,
-}
-
-#[derive(Clone, Copy)]
-enum DesiredCapacity {
-    Fraction(f32),
-    Multiple(usize),
-}
-
-impl DesiredCapacity {
-    fn capacity(&self) -> f32 {
-        match self {
-            DesiredCapacity::Fraction(f) => *f,
-            DesiredCapacity::Multiple(_) => 1.0,
-        }
-    }
-}
-
-/// AcquiredGPUs is a vector of GPU indices that are acquired.
-/// It can be 1 or many, but can never be 0.
-type AcquiredGPUs = Vec<usize>;
 
 impl GPUPool {
     pub fn new(num_gpus: usize) -> Self {
         assert!(num_gpus >= 1, "num_gpus must be >= 1, got {num_gpus}");
         let state = GPUPoolState {
             capacity: vec![1.0; num_gpus],
-            waiters: LinkedList::new(),
+            reserved: std::iter::repeat_with(VecDeque::new)
+                .take(num_gpus)
+                .collect(),
         };
         GPUPool {
             num_gpus,
@@ -66,37 +43,55 @@ impl GPUPool {
 
     pub async fn acquire(&self, fraction: f32) -> usize {
         let mut state = self.state.lock().await;
-        if let Some(index) = Self::find_available(&state.capacity, fraction) {
-            state.capacity[index] -= fraction;
-            return index;
+        if let Some(gpu) = Self::find_available(&state.capacity, fraction, &state.reserved) {
+            state.capacity[gpu] -= fraction;
+            return gpu;
         }
-        let (sender, recv) = oneshot::channel();
-        state.waiters.push_back(Waiter {
-            desired_capacity: DesiredCapacity::Fraction(fraction),
-            sender,
-        });
+        let (reserved_gpu, recv) = Self::reserve_gpu(fraction, &mut state);
         drop(state);
         match recv.await {
-            Ok(mut gpus) => {
-                debug_assert!(
-                    gpus.len() == 1,
-                    "Expected a fraction of a single GPU, but received multiple: {gpus:?}"
-                );
-                gpus.pop().unwrap()
-            }
+            Ok(()) => reserved_gpu,
             Err(err) => panic!("GPUPool dropped while waiting: {err}"),
         }
     }
 
-    fn find_available(capacity: &[f32], fraction: f32) -> Option<usize> {
+    fn find_available<T>(
+        capacity: &[f32],
+        fraction: f32,
+        exclude: &[VecDeque<T>],
+    ) -> Option<usize> {
         let max_position = capacity
             .iter()
-            .position_max_by(|a, b| a.partial_cmp(b).unwrap())?;
-        if capacity[max_position] >= fraction {
-            Some(max_position)
-        } else {
-            None
-        }
+            .enumerate()
+            .filter(|(gpu_id, _)| exclude[*gpu_id].is_empty())
+            .max_by(|(_, cap_a), (_, cap_b)| cap_a.partial_cmp(cap_b).unwrap())
+            .map(|(gpu_id, _)| gpu_id)?;
+        (capacity[max_position] >= fraction).then_some(max_position)
+    }
+
+    fn reserve_gpu(fraction: f32, state: &mut GPUPoolState) -> (usize, oneshot::Receiver<()>) {
+        Self::reserve_gpu_with_exclusion(fraction, state, &HashSet::new())
+    }
+
+    fn reserve_gpu_with_exclusion(
+        fraction: f32,
+        state: &mut GPUPoolState,
+        exclude_gpus: &HashSet<usize>,
+    ) -> (usize, oneshot::Receiver<()>) {
+        let (sender, recv) = oneshot::channel();
+        let reserved_gpu = Self::find_shortest_queue(&state.reserved, exclude_gpus);
+        state.reserved[reserved_gpu].push_back((fraction, sender));
+        (reserved_gpu, recv)
+    }
+
+    fn find_shortest_queue<T>(queues: &[VecDeque<T>], exclude: &HashSet<usize>) -> usize {
+        queues
+            .iter()
+            .enumerate()
+            .filter(|(gpu_id, _)| !exclude.contains(gpu_id))
+            .min_by(|(_, queue_a), (_, queue_b)| queue_a.len().cmp(&queue_b.len()))
+            .map(|(gpu_id, _)| gpu_id)
+            .unwrap_or_default()
     }
 
     /// Acquires a given integer number of fully available GPUs (capacity == 1.0) from the GPU pool.
@@ -118,83 +113,75 @@ impl GPUPool {
             ));
         }
         let mut state = self.state.lock().await;
-        let indexes = Self::find_fully_available(&state.capacity, gpu_count);
-        if indexes.len() >= gpu_count {
-            for position in &indexes[..gpu_count] {
-                state.capacity[*position] = 0_f32;
-            }
-            return Ok(indexes);
+        let gpu_ids = Self::find_fully_available(&state.capacity, gpu_count, &state.reserved);
+        let acquired_gpu_count = gpu_ids.len();
+        for gpu_id in &gpu_ids {
+            state.capacity[*gpu_id] = 0_f32;
         }
-        let (sender, recv) = oneshot::channel();
-        state.waiters.push_back(Waiter {
-            desired_capacity: DesiredCapacity::Multiple(gpu_count),
-            sender,
-        });
+        if acquired_gpu_count == gpu_count {
+            return Ok(gpu_ids);
+        }
+        let reserved_gpu_count = gpu_count - acquired_gpu_count;
+        let mut acquired_gpus = gpu_ids;
+        let exclude_gpus: HashSet<_> = acquired_gpus.clone().into_iter().collect();
+        let mut pending_gpus = Vec::with_capacity(reserved_gpu_count);
+        let mut pending_tasks = Vec::with_capacity(reserved_gpu_count);
+        for _ in 0..reserved_gpu_count {
+            let (gpu_id, task) = Self::reserve_gpu_with_exclusion(1.0, &mut state, &exclude_gpus);
+            pending_gpus.push(gpu_id);
+            pending_tasks.push(task);
+        }
         drop(state);
-        match recv.await {
-            Ok(gpus) => {
-                debug_assert!(
-                    gpus.len() == gpu_count,
-                    "Expected {} GPUs, but received {}",
-                    gpu_count,
-                    gpus.len()
-                );
-                Ok(gpus)
-            }
-            Err(err) => panic!("GPUPool dropped while waiting: {err}"),
-        }
+        futures::future::join_all(pending_tasks)
+            .await
+            .into_iter()
+            .collect::<Result<Vec<_>, _>>()?;
+        acquired_gpus.extend(pending_gpus);
+        Ok(acquired_gpus)
     }
 
-    fn find_fully_available(capacity: &[f32], count: usize) -> Vec<usize> {
+    fn find_fully_available<T>(
+        capacity: &[f32],
+        count: usize,
+        exclude: &[VecDeque<T>],
+    ) -> Vec<usize> {
         debug_assert!(count >= 1, "count must be >= 1, got {count}");
         capacity
             .iter()
-            .positions(|cap| (*cap - 1.0).abs() < f32::EPSILON)
+            .enumerate()
+            .filter(|(gpu_id, _)| exclude[*gpu_id].is_empty())
+            .filter(|(_, cap)| (*cap - 1.0).abs() <= f32::EPSILON)
+            .map(|(gpu_id, _)| gpu_id)
             .take(count)
             .collect()
     }
 
     pub async fn release(&self, gpu_id: usize, fraction: f32) {
+        debug_assert!(gpu_id < self.num_gpus(), "gpu_id out of bounds");
+        debug_assert!(
+            fraction > 0.0,
+            "fraction must be positive, got {}",
+            fraction
+        );
         let mut state = self.state.lock().await;
         state.capacity[gpu_id] += fraction;
-        let length = state.waiters.len();
-        for _ in 0..length {
-            let waiter = state.waiters.pop_front().unwrap();
-            if waiter.sender.is_closed() {
-                continue;
-            }
-            match Self::fulfill_capacity(&state.capacity, waiter.desired_capacity) {
-                Some(gpus) => {
-                    if waiter.sender.send(gpus.clone()).is_ok() {
-                        // if fails to send, it means it's no longer waiting.
-                        for gpu in gpus {
-                            state.capacity[gpu] -= waiter.desired_capacity.capacity();
-                        }
-                    }
-                }
-                None => state.waiters.push_back(waiter),
-            }
-        }
-    }
-
-    fn fulfill_capacity(
-        capacities: &[f32],
-        desired_capacity: DesiredCapacity,
-    ) -> Option<AcquiredGPUs> {
-        match desired_capacity {
-            DesiredCapacity::Fraction(fraction) => {
-                if let Some(gpu) = Self::find_available(capacities, fraction) {
-                    return Some(vec![gpu]);
-                }
-            }
-            DesiredCapacity::Multiple(count) => {
-                let gpus = Self::find_fully_available(capacities, count);
-                if gpus.len() == count {
-                    return Some(gpus);
-                }
+        debug_assert!(
+            (state.capacity[gpu_id]) <= 1.0 + f32::EPSILON,
+            "capacity overflown"
+        );
+        while state.reserved[gpu_id]
+            .front()
+            .map(|(fraction, _)| f32::EPSILON >= state.capacity[gpu_id] - *fraction)
+            .unwrap_or(false)
+        {
+            let Some((fraction, sender)) = state.reserved[gpu_id].pop_front() else {
+                break;
+            };
+            if sender.send(()).is_ok() {
+                state.capacity[gpu_id] -= fraction;
+                break;
             }
         }
-        None
     }
 
     /// detect the number of GPUs available for the default pool.
@@ -399,12 +386,12 @@ mod tests {
                 .acquire_full(NonZeroUsize::new(3).expect("3 is not zero"))
                 .await
         });
+        let cloned_pool = pool.clone();
+        let second_acquired_gpu = tokio::spawn(async move { cloned_pool.acquire(0.2).await });
         tokio::time::sleep(std::time::Duration::from_secs_f32(0.02)).await;
         assert!(!task.is_finished());
-        let second_acquired_gpu = pool.acquire(0.5).await;
-        assert_eq!(second_acquired_gpu, 1);
+        assert!(!second_acquired_gpu.is_finished());
         pool.release(partially_used_gpu, 0.6).await;
-        pool.release(second_acquired_gpu, 0.5).await;
         let result = tokio::time::timeout(std::time::Duration::from_secs(1), task)
             .await
             .expect("task finished")
@@ -426,6 +413,102 @@ mod tests {
             result.unwrap_err().to_string(),
             "Attempted to acquire 3 GPUs but only has 2."
         );
+    }
+
+    #[tokio::test]
+    async fn test_reserve_gpus_then_release() {
+        let pool = Arc::new(GPUPool::new(2));
+        let gpu_0 = pool.acquire(0.5).await;
+        assert_eq!(gpu_0, 1);
+        let gpu_1 = pool.acquire(0.6).await;
+        assert_eq!(gpu_1, 0);
+        let cloned_pool = pool.clone();
+        let reserving_task_1 = tokio::spawn(async move { cloned_pool.acquire(0.6).await });
+        let cloned_pool = pool.clone();
+        let reserving_task_2 = tokio::spawn(async move { cloned_pool.acquire(0.7).await });
+        tokio::time::sleep(std::time::Duration::from_secs_f32(0.02)).await;
+        assert!(!reserving_task_1.is_finished());
+        assert!(!reserving_task_2.is_finished());
+
+        pool.release(gpu_1, 0.2).await;
+        let reserving_task_1_acquired_gpu =
+            tokio::time::timeout(std::time::Duration::from_secs(1), reserving_task_1)
+                .await
+                .expect("task finished")
+                .expect("no timeout");
+        assert_eq!(reserving_task_1_acquired_gpu, gpu_1);
+        assert!(!reserving_task_2.is_finished());
+
+        pool.release(gpu_0, 0.2).await;
+        let reserving_task_2_acquired_gpu =
+            tokio::time::timeout(std::time::Duration::from_secs(1), reserving_task_2)
+                .await
+                .expect("task finished")
+                .expect("no timeout");
+        assert_eq!(reserving_task_2_acquired_gpu, gpu_0);
+
+        pool.release(gpu_0, 1.0).await;
+        pool.release(gpu_1, 1.0).await;
+    }
+
+    #[tokio::test]
+    async fn test_reserve_gpus_without_affecting_unreserved() {
+        let pool = Arc::new(GPUPool::new(2));
+        let gpu_0 = pool.acquire(0.5).await;
+        assert_eq!(gpu_0, 1);
+        let gpu_1 = pool.acquire(0.6).await;
+        assert_eq!(gpu_1, 0);
+        let cloned_pool = pool.clone();
+        let reserving_task = tokio::spawn(async move { cloned_pool.acquire(0.6).await });
+        let cloned_pool = pool.clone();
+        let task_not_blocked = tokio::spawn(async move { cloned_pool.acquire(0.2).await });
+        tokio::time::sleep(std::time::Duration::from_secs_f32(0.02)).await;
+        assert!(!reserving_task.is_finished());
+        assert!(task_not_blocked.is_finished());
+        pool.release(gpu_0, 0.7).await;
+
+        pool.release(gpu_1, 0.2).await;
+        let reserving_task_acquired_gpu =
+            tokio::time::timeout(std::time::Duration::from_secs(1), reserving_task)
+                .await
+                .expect("task finished")
+                .expect("no timeout");
+        assert_eq!(reserving_task_acquired_gpu, gpu_1);
+
+        pool.release(gpu_1, 1.0).await;
+    }
+
+    #[tokio::test]
+    async fn test_reserve_the_same_gpu_in_a_queue() {
+        let pool = Arc::new(GPUPool::new(1));
+        let gpu_0 = pool.acquire(0.5).await;
+        assert_eq!(gpu_0, 0);
+        let cloned_pool = pool.clone();
+        let reserving_task_1 = tokio::spawn(async move { cloned_pool.acquire(0.6).await });
+        let cloned_pool = pool.clone();
+        let reserving_task_2 = tokio::spawn(async move { cloned_pool.acquire(0.7).await });
+        tokio::time::sleep(std::time::Duration::from_secs_f32(0.02)).await;
+        assert!(!reserving_task_1.is_finished());
+        assert!(!reserving_task_2.is_finished());
+
+        pool.release(gpu_0, 0.1).await;
+        let reserving_task_1_acquired_gpu =
+            tokio::time::timeout(std::time::Duration::from_secs(1), reserving_task_1)
+                .await
+                .expect("task finished")
+                .expect("no timeout");
+        assert_eq!(reserving_task_1_acquired_gpu, gpu_0);
+        assert!(!reserving_task_2.is_finished());
+
+        pool.release(gpu_0, 0.7).await;
+        let reserving_task_2_acquired_gpu =
+            tokio::time::timeout(std::time::Duration::from_secs(1), reserving_task_2)
+                .await
+                .expect("task finished")
+                .expect("no timeout");
+        assert_eq!(reserving_task_2_acquired_gpu, gpu_0);
+
+        pool.release(gpu_0, 1.0).await;
     }
 
     #[test]
